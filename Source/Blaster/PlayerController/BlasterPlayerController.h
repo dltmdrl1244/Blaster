f19cb093ed0c78b5f5ae16aff606c7c5e7b46b27
dltmdrl1244 <dltmdrl1244@naver.com>
2024-07-03 23:46:26 +0900

    Update: Sync Game Timer

--- Source/Blaster/PlayerController/BlasterPlayerController.h
+++ Source/Blaster/PlayerController/BlasterPlayerController.h
@@ -20,11 +20,37 @@ public:
 	void SetHUDDefeats(int32 Defeats);
 	void SetHUDWeaponAmmo(int32 Ammo);
 	void SetHUDCarriedAmmo(int32 Ammo);
+	void SetHUDMatchCountdown(float CountdownTime);
 	virtual void OnPossess(APawn* InPawn) override;
+	virtual void Tick(float DeltaTime) override;
+
+	virtual float GetServerTime(); // synced with server world clock
+	virtual void ReceivedPlayer() override;
 protected:
 	virtual void BeginPlay() override;
+	void SetHUDTime();
 
+	/*
+	Sync time between server-client
+	*/
+
+	// request the current server time passing in client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+	// reports the current server time to the client in response to ServerRequestServerTime()
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+
+	float ClientServerDelta = 0.f; // Difference between client and server time
+	UPROPERTY(EditAnywhere, Category = Time)
+	float TimeSyncFrequency = 5.f;
+	float TimeSyncRunningTime = 0.f;
+
+	void CheckTimeSync(float DeltaTime);
 
 private:
 	class ABlasterHUD* BlasterHUD;
+
+	float MatchTime = 120.f;
+	uint32 CountdownInt;
 };
