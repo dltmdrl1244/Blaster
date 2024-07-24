8cc01f9b9324eee6ed3726cc71274e977eef61d0
dltmdrl1244 <dltmdrl1244@naver.com>
2024-07-24 21:11:18 +0900

    Server Rewind: Package History

---
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LagCompensationComponent.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Components/BoxComponent.h"
+#include "DrawDebugHelpers.h"
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FFramePackage Package;
+	SaveFramePackage(Package);
+	ShowFramePackage(Package, FColor::Orange);
+}
+
+void ULagCompensationComponent::SaveFramePackage(FFramePackage& Package)
+{
+	Character = Character == nullptr ? Cast<ABlasterCharacter>(GetOwner()) : Character;
+	if (Character)
+	{
+		Package.Time = GetWorld()->GetTimeSeconds();
+		for (auto& Iter : Character->HitCollisionBoxes)
+		{
+			UBoxComponent* Box = Iter.Value;
+			FBoxInformation BoxInformation;
+			BoxInformation.Location = Box->GetComponentLocation();
+			BoxInformation.Rotation = Box->GetComponentRotation();
+			BoxInformation.BoxExtent = Box->GetScaledBoxExtent();
+
+			Package.HitBoxInfo.Add(Iter.Key, BoxInformation);
+		}
+	}
+}
+
+void ULagCompensationComponent::ShowFramePackage(const FFramePackage Package, const FColor& Color)
+{
+	for (auto& BoxInfo : Package.HitBoxInfo)
+	{
+		DrawDebugBox(
+			GetWorld(),
+			BoxInfo.Value.Location,
+			BoxInfo.Value.BoxExtent,
+			FQuat(BoxInfo.Value.Rotation),
+			Color,
+			false,
+			4.f
+		);
+	}
+}
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (FrameHistory.Num() <= 1)
+	{
+		FFramePackage ThisFrame;
+		SaveFramePackage(ThisFrame);
+		FrameHistory.AddHead(ThisFrame);
+	}
+	else
+	{
+		float HistoryLength = FrameHistory.GetHead()->GetValue().Time - FrameHistory.GetTail()->GetValue().Time;
+		while (HistoryLength > MaxRecordTime)
+		{
+			FrameHistory.RemoveNode(FrameHistory.GetTail());
+			HistoryLength = FrameHistory.GetHead()->GetValue().Time - FrameHistory.GetTail()->GetValue().Time;
+		}
+		FFramePackage ThisFrame;
+		SaveFramePackage(ThisFrame);
+		FrameHistory.AddHead(ThisFrame);
+
+		ShowFramePackage(ThisFrame, FColor::Orange);
+	}
+}
